92823ff70118f07388d344d996872fcbd0a86ff0
null221 <agd1923@gmail.com>
2025-08-13 14:08:21 +0200

    added default values for inputs

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -1,6 +1,7 @@
-import { Link, Form, useActionData, type ActionFunctionArgs, redirect } from "react-router-dom"
+import { Link, Form, useActionData, type ActionFunctionArgs, redirect, useLocation } from "react-router-dom"
 import ErrorMessage from "../components/ErrorMessage"
 import { addProduct } from "../services/ProductService"
+import ProductDetails from "../components/ProductDetails"
 
 
 export async function action({request} : ActionFunctionArgs) {
@@ -17,7 +18,7 @@ export async function action({request} : ActionFunctionArgs) {
         return error
     }
 
-    await addProduct(data)
+    //await addProduct(data)
 
     return redirect('/')
 }
@@ -27,7 +28,9 @@ export default function EditProduct() {
 
 
     const error = useActionData() as string
-    console.log(error)
+    const {state} = useLocation()
+
+    console.log(state)
 
   return (
     <>
@@ -60,6 +63,7 @@ export default function EditProduct() {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Nombre del Producto"
                     name="name"
+                    defaultValue={state.product.name}
                 />
             </div>
             <div className="mb-4">
@@ -73,6 +77,7 @@ export default function EditProduct() {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Precio Producto. ej. 200, 300"
                     name="price"
+                    defaultValue={state.product.price}
                 />
             </div>
             <input
